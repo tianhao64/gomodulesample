2918386e78649bec65ad97d3dc35502cbbc4b994
Tianhao He <het@vmware.com>
2019-12-17 22:55:55 +0800

    fix format

--- hello.go
+++ hello.go
@@ -1,22 +1,23 @@
 package hello
 
 import (
-    "rsc.io/quote/v3"
-    "runtime"
 	"fmt"
+	"runtime"
+
+	"rsc.io/quote/v3"
 )
 
 func Hello() string {
-    return quote.HelloV3()
+	return quote.HelloV3()
 }
 
 func Proverb() string {
-    return quote.Concurrency()
+	return quote.Concurrency()
 }
 
 func main() string {
-    sdkVersion := "1.0.0"
+	sdkVersion := "1.0.0"
 	runtimeVersion := "2.14.0"
-    fmt.Printf("%s vAPI %s %s (%s; %s)", sdkVersion, runtimeVersion, runtime.Version(), runtime.GOOS, runtime.GOARCH, )
-    return ""
+	fmt.Printf("%s vAPI %s %s (%s; %s)", sdkVersion, runtimeVersion, runtime.Version(), runtime.GOOS, runtime.GOARCH)
+	return ""
 }
